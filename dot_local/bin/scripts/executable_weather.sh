9950b929b10119b4d0d9f50e3f6b9ff53a869500
avjinder <avisekhon@gmail.com>
2022-12-16 17:00:04 +0530

    updated some files

--- dot_local/bin/scripts/executable_weather.sh
+++ dot_local/bin/scripts/executable_weather.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wttr="$(curl -s -L wttr.in/Amritsar?format=1)"
-echo "${wttr##* }"
+wttr="$(curl -s -L wttr.in/{amritsar,calgary}?format=1 | awk '{ print $1, $NF }' | tr '\n' ' ')"
+echo "$wttr"
 
 
