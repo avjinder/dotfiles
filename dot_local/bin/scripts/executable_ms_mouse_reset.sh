d04b90a5cc8693d0300fc31d820467dd11d46b46
avjinder <avisekhon@gmail.com>
2023-01-31 18:16:40 +0530

    changed some stuff

---
+++ dot_local/bin/scripts/executable_ms_mouse_reset.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+mouse_ports="$(dmesg -k | grep 'Manufacturer: Microsoft' | tail -n 1 | awk '{print $3}' | sed 's/://g')"
+if [[ -n "$mouse_ports" ]]; then
+	sh -c "echo $mouse_ports > /sys/bus/usb/drivers/usb/unbind"
+	sh -c "echo $mouse_ports > /sys/bus/usb/drivers/usb/bind"
+	echo "reset ms_mouse"
+fi
