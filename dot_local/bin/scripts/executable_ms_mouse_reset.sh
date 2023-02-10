2fa3c212213fa5911df957e5fc369b579e9ce9d6
avjinder <avisekhon@gmail.com>
2023-02-10 16:13:17 +0530

    some changes

--- dot_local/bin/scripts/executable_ms_mouse_reset.sh
+++ dot_local/bin/scripts/executable_ms_mouse_reset.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-mouse_ports="$(dmesg -k | grep 'Manufacturer: Microsoft' | tail -n 1 | awk '{print $3}' | sed 's/://g')"
+mouse_ports="$(dmesg -k | grep 'Manufacturer: Microsoft' | tail -n 1 | awk '{print $4}' | sed 's/://g')"
 if [[ -n "$mouse_ports" ]]; then
 	sh -c "echo $mouse_ports > /sys/bus/usb/drivers/usb/unbind"
 	sh -c "echo $mouse_ports > /sys/bus/usb/drivers/usb/bind"
