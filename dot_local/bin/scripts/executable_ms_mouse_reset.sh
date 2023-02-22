8e3f6cd814134b7cacda37111de8497a756c5314
avjinder <avisekhon@gmail.com>
2023-02-22 11:57:39 +0530

    Added debug mode to script to help with debugging

--- dot_local/bin/scripts/executable_ms_mouse_reset.sh
+++ dot_local/bin/scripts/executable_ms_mouse_reset.sh
@@ -1,8 +1,34 @@
 #!/usr/bin/bash
 
-mouse_ports="$(dmesg -k | grep 'Manufacturer: Microsoft' | tail -n 1 | awk '{print $4}' | sed 's/://g')"
-if [[ -n "$mouse_ports" ]]; then
-	sh -c "echo $mouse_ports > /sys/bus/usb/drivers/usb/unbind"
-	sh -c "echo $mouse_ports > /sys/bus/usb/drivers/usb/bind"
-	echo "reset ms_mouse"
-fi
+last_entry="$(dmesg -k | grep 'Manufacturer: Microsoft' | tail -n 1)"
+mouse_field="$(echo "$last_entry" | awk '{print $3}' | sed 's/://g')"
+
+# resets the passed in port
+function reset_ports() {
+	if [[ -n "$1" ]]; then
+		sh -c "echo $1 > /sys/bus/usb/drivers/usb/unbind"
+		sh -c "echo $1 > /sys/bus/usb/drivers/usb/bind"
+		echo "reset ms_mouse successful"
+	fi
+}
+
+# shows detailed information useful while debugging
+function debug() {
+	echo "Mouse Refresh Entry:"
+	printf "%s\n" "$last_entry"
+	printf "%s\n" "$mouse_field"
+}
+
+case $1 in 
+	"debug")
+		debug
+		;;
+	*)
+		if [[ "$EUID" -ne 0 ]]; then
+			echo "error: please run as root"
+			exit 1
+		fi
+		reset_ports "$mouse_field"
+		;;
+esac
+
