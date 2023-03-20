d69ce279dfaecc8cb360491585d8afc4bbc905a0
avjinder <avisekhon@gmail.com>
2023-03-20 12:15:17 +0530

    minor changes

--- dot_local/bin/scripts/executable_ms_mouse_reset.sh
+++ dot_local/bin/scripts/executable_ms_mouse_reset.sh
@@ -5,11 +5,20 @@ mouse_field="$(echo "$last_entry" | awk '{print $3}' | sed 's/://g')"
 
 # resets the passed in port
 function reset_ports() {
-	if [[ -n "$1" ]]; then
-		sh -c "echo $1 > /sys/bus/usb/drivers/usb/unbind"
-		sh -c "echo $1 > /sys/bus/usb/drivers/usb/bind"
-		echo "reset ms_mouse successful"
-	fi
+	port="1-3"
+	sh -c "echo $port > /sys/bus/usb/drivers/usb/unbind"
+	sh -c "echo $port > /sys/bus/usb/drivers/usb/bind"
+	echo "reset ms_mouse successful"
+	# port="$1"
+	# if [[ -z "$port" ]]; then
+	# 	echo "Microsoft mouse port not found, setting to default 1-3"
+	# 	port="1-3"
+	# fi
+	# 	sh -c "echo $port > /sys/bus/usb/drivers/usb/unbind"
+	# 	sh -c "echo $port > /sys/bus/usb/drivers/usb/bind"
+	# 	echo "reset ms_mouse successful"
+	# if [[ -n "$1" ]]; then
+	# fi
 }
 
 # shows detailed information useful while debugging
